b6a57059bf0ed757c6318bd7aeb299a267a1b2df
Arslan <arslanarsal455@gmail.com>
2024-01-14 14:16:46 +0500

    Weekly contest

---
+++ Leetcode/Contest/Weekly Contest 380/Count_Elements_With_Maximum_Frequency.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxFrequencyElements(vector<int> &nums)
+    {
+        vector<int> freq(101, 0);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            freq[nums[i]]++;
+        }
+        int ans = 0;
+        int mafreq = 0;
+        for (int i = 1; i < 100; i++)
+        {
+            if (nums[i] == mafreq)
+            {
+                ans += nums[i];
+            }
+            else if (nums[i] > mafreq)
+            {
+                ans = nums[i];
+                mafreq = i;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
