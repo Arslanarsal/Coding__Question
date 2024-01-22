df3b232f42041d2cc94dc6f36ab251529c949d27
Arslan <arslanarsal455@gmail.com>
2024-01-23 00:12:55 +0500

    Daily problem

---
+++ Leetcode/Leetcode DailyProblem/january/Set_Mismatch.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> findErrorNums(vector<int> &nums)
+    {
+        int missing, repeat;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[abs(nums[i]) - 1] < 0)
+                repeat = abs(nums[i]);
+            else
+                nums[abs(nums[i]) - 1] *= -1;
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0)
+                missing = i + 1;
+        }
+
+        return {repeat, missing};
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
