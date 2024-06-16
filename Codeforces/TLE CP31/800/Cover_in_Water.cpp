92c8b3bc7d56c3d43549e17caeb3fa39746621d6
Arslan <arslanarsal455@gmail.com>
2024-06-16 13:57:18 +0500

    tle

--- Codeforces/TLE CP31/800/Cover_in_Water.cpp
+++ Codeforces/TLE CP31/800/Cover_in_Water.cpp
@@ -1,41 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define M 1000000007
+
+// Mathematical functions
+long long mod_sub(long long a, long long b, long long m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+long long powermod(long long x, long long y, long long p)
+{
+    long long res = 1;
+    x = x % p;
+    if (x == 0)
+        return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        int n;
-        string s;
-        cin >> n >> s;
-        int cur = 0;
-        int ans = 0;
-
-        for (int i = 0; i < n; i++)
+        long long l, r, k;
+        cin >> l >> r >> k;
+        if (k >= 10)
         {
-            if (s[i] == '.')
-            {
-                cur++;
-            }
-            if (cur > 2)
-            {
-                ans = 2;
-                break;
-            }
-            if (s[i] == '#')
-            {
-                ans += cur;
-                cur = 0;
-            }
+            cout << 0 << endl;
         }
-        if (cur < 3 && s[n - 1] == '.')
+        else if (k >= 5)
         {
-            ans += cur;
+            long long ls = powermod(2, l, M); // log(l)
+            long long rs = powermod(2, r, M); // log(r)
+            cout << mod_sub(rs, ls, M) << endl;
         }
-        cout << ans << "\n";
+        else if (k == 4)
+        {
+            long long ls = powermod(3, l, M);
+            long long rs = powermod(3, r, M);
+            cout << mod_sub(rs, ls, M) << endl;
+        }
+        else if (k == 3)
+        {
+            long long ls = powermod(4, l, M);
+            long long rs = powermod(4, r, M);
+            cout << mod_sub(rs, ls, M) << endl;
+        }
+        else if (k == 2)
+        {
+            long long ls = powermod(5, l, M);
+            long long rs = powermod(5, r, M);
+            cout << mod_sub(rs, ls, M) << endl;
+        }
+        else
+        {
+            long long ls = powermod(10, l, M);
+            long long rs = powermod(10, r, M);
+            cout << mod_sub(rs, ls, M) << endl;
+        }
     }
-
     return 0;
 }
+
+// TC - log(r)
+// SC - O(1)
