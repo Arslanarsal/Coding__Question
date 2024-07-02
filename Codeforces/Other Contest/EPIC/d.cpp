08525270274b267c8d34c4fe2c404de9bfdc2db1
Arslan <arslanarsal455@gmail.com>
2024-07-02 10:19:21 +0500

    codeforce

--- Codeforces/Other Contest/EPIC/d.cpp
+++ Codeforces/Other Contest/EPIC/d.cpp
@@ -13,12 +13,12 @@ int solve(int index, int remaining, vector<int> &values)
 {
     if (index >= n)
         return 0;
-        
-    int &result = dp[index][remaining];
-    if (visited[index][remaining] == current_id)
-        return result;
-    visited[index][remaining] = current_id;
-    result = solve(index + 1, remaining + 1, values);
+
+    // int &result = dp[index][remaining];
+    // if (visited[index][remaining] == current_id)
+    //     return result;
+    // visited[index][remaining] = current_id;
+    int result = solve(index + 1, remaining + 1, values);
     if (remaining >= frequency[values[index]])
     {
         result = max(result, 1 + solve(index + 1, remaining - frequency[values[index]], values));
@@ -38,10 +38,10 @@ void solve_test_case()
         cin >> x;
         value_counts[x]++;
     }
-    for (auto [value, count] : value_counts)
+    for (auto it : value_counts)
     {
-        frequency[value] = count;
-        values.push_back(value);
+        frequency[it.first] = it.second;
+        values.push_back(it.first);
     }
     n = values.size();
     cout << n - solve(1, 1, values) << '\n';
@@ -61,3 +61,82 @@ int main()
         solve_test_case();
     }
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int solve(int ind, vector<int> &freq, bool turn, int &n)
+// {
+//     if (ind == n)
+//     {
+//         return 0;
+//     }
+
+
+//     if (turn)
+//     {
+//         for (int i = ind; i < n; i++)
+//         {
+//             if (freq[i])
+//             {
+//                 return 1 + solve(i + 1, freq, !turn, n);
+//             }
+//         }
+//     }
+//     else
+//     {
+//         int temp = INT_MAX;
+//         int j = 0;
+//         for (int i = ind; i < n; i++)
+//         {
+//             if (freq[i])
+//             {
+//                 freq[i]--;
+//                 int ret = solve(ind, freq, !turn, n);
+//                 if (ret < temp)
+//                 {
+//                     temp = ret;
+//                     j = i;
+//                 }
+
+//                 freq[i]++;
+//             }
+//         }
+//         if (temp == INT_MAX)
+//         {
+//             return  0;
+//         }
+//         return temp;
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int n;
+//         cin >> n;
+//         int arr[n];
+//         map<int, int> mp;
+//         for (int i = 0; i < n; i++)
+//         {
+//             int x;
+//             cin >> x;
+//             arr[i] = x;
+//             mp[x]++;
+//         }
+//         vector<int> freq;
+//         for (auto &&it : mp)
+//         {
+//             freq.push_back(it.second);
+//         }
+//         int m = freq.size();
+//         int ans = solve(0, freq, true, m);
+//         cout << ans << "\n";
+//     }
+
+//     return 0;
+// }
