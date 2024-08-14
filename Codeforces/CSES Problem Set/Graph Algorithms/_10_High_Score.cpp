612afbf91b3e5e60d4bf1d147177866db4ca57f6
Arslan <arslanarsal455@gmail.com>
2024-08-14 18:12:37 +0500

    cycle

---
+++ Codeforces/CSES Problem Set/Graph Algorithms/_10_High_Score.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF LLONG_MAX   
+#define int long long int
+vector<vector<int>> adj, a, b;
+vector<int> dist, vist, vist1;
+int flag = 0;
+
+void bellmanford(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        flag = 0;
+        for (auto &&it : adj)
+        {
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            if (dist[u] != INF && vist1[v] && vist[u] && dist[u] + w < dist[v])
+            {
+                flag = 1;
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+}
+
+void dfs(int u, vector<int> &v, vector<vector<int>> &g)
+{
+    v[u] = 1;
+    for (auto &&e : g[u])
+    {
+        if (!v[e])
+        {
+            dfs(e, v, g);
+        }
+    }
+}
+
+int32_t main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    adj.resize(m);
+    a.resize(n + 1);
+    b.resize(n + 1);
+    vist.resize(n + 1, 0);
+    vist1.resize(n + 1, 0);
+    dist.resize(n + 1, INF);
+    dist[1] = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        a[u].push_back(v);
+        b[v].push_back(u);
+        adj[i] = {u, v, -wt};
+    }
+    dfs(1, vist, a);
+    dfs(n, vist1, b);
+    bellmanford(n);
+    if (flag)
+    {
+        cout << -1 << "\n";
+    }
+    else
+    {
+
+        cout << dist[n] * -1 << "\n";
+    }
+    return 0;
+}
---
+++ Codeforces/CSES Problem Set/Graph Algorithms/_12_Cycle_Finding.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF LLONG_MAX
+#define int long long int
+#define t tuple<int, int, int>
+int n, m;
+vector<int> dist;
+vector<t> adj;
+vector<int> relection;
+int x;
+
+void bellman_ford()
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        x = -1;
+        for (auto &&it : adj)
+        {
+            int u = get<0>(it);
+            int v = get<1>(it);
+            int w = get<2>(it);
+            if (dist[u] < INF && dist[v] > dist[u] + w)
+            {
+                dist[v] = dist[u] + w;
+                relection[v] = u;
+                x = v;
+            }
+        }
+    }
+}
+
+int32_t main()
+{
+    cin >> n >> m;
+    dist.resize(n + 1, 0);
+    relection.resize(n + 1, -1);
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj.push_back({u, v, w});
+    }
+
+    bellman_ford();
+
+    if (x == -1)
+    {
+        cout << "NO\n";
+        return 0;
+    }
+
+    // Re-run to find the exact cycle
+    for (int i = 1; i <= n; i++)
+    {
+        x = relection[x];
+    }
+
+    vector<int> cycle;
+    int start = x;
+    do
+    {
+        cycle.push_back(x);
+        x = relection[x];
+    } while (x != start);
+    cycle.push_back(start);
+
+    reverse(cycle.begin(), cycle.end());
+
+    cout << "YES\n";
+    int j = cycle.size();
+    for (int i = 0; i < j; i++)
+    {
+        cout << cycle[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
