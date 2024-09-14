64a755acaa55e026bc2698f17a4039d106f226e9
Arslan <arslanarsal455@gmail.com>
2024-09-14 13:52:32 +0500

    contest

--- Leetcode/Contest/C.cpp
+++ Leetcode/Contest/C.cpp
@@ -3,33 +3,16 @@ using namespace std;
 
 class Solution
 {
-public:
-    long long maximumValueSum(vector<vector<int>> &board)
+    int phi(int n)
     {
-        int n = board.size();
-        int m = board[0].size();
-        vector<vector<int>> arr(n, vector<int>(3, 0));
         for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < n; j++)
-            {
-                sort(board[i].begin(), board[i].end());
-            }
         }
-        int ans = INT_MIN;
-        for (int i = 0; i < n - 3; i++)
-        {
-            for (int j = m - 3; j < m; j++)
-            {
-                int temp = board[i][j];
-                for (int i = 0; i < count; i++)
-                {
-                    /* code */
-                }
-                
-            }
-        }
-        return 0;
+    }
+
+public:
+    int superPow(int a, vector<int> &b)
+    {
     }
 };
 
