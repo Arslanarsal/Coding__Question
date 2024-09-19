6bedc039039ac5d5250d55501d483464facc6c85
Arslan <arslanarsal455@gmail.com>
2024-09-19 13:30:19 +0500

    dailt problem

--- Leetcode/DailyProblem/_10_.cpp
+++ Leetcode/DailyProblem/_10_.cpp
@@ -1,59 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution
 {
 public:
-    vector<ListNode *> splitListToParts(ListNode *head, int k)
+    string largestNumber(vector<int> &nums)
     {
-        vector<ListNode *> ans(k);
-        ListNode *temp = head;
-        int num = 0;
-        while (temp)
-        {
-            num++;
-            temp = temp->next;
-        }
-        int block = num / k;
-        int rem = num % k;
-        int i = 0;
-        ListNode *last, first;
-        while (head)
-        {
-            int j = 0;
-            ans[i] = head;
-            i++;
-            last = head;
-            while (last && j < block)
-            {
-                head = last;
-                last = last->next;
-                j++;
-            }
-            if (last && rem)
-            {
-                head = last;
-                last = last->next;
-                rem--;
-            }
-            head->next = NULL;
-            head = last;
-        }
-        return ans;
     }
 };
 
 int main()
 {
+    vector<string> s = {"9", "93", "90", "949"};
+    sort(s.begin(), s.end());
+    for (auto &&i : s)
+    {
+        cout << i << " ";
+    }
 
     return 0;
 }
